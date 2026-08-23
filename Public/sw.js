854c7ffb3da2492e3c68e456b221ebb771771ceb
visheshprobro23 <mishrarakesh1393@gmail.com>
2026-08-23 21:28:08 +0530

    Add files via upload

---
+++ Public/sw.js
@@ -0,0 +1,1 @@
+const C='exam-aspirant-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.add('/'))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
